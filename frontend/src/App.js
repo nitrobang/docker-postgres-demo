291237adc575d259f4a814b09efb0455ea3759cb
nitrobang <kartikstudy0970@gmail.com>
2025-09-19 15:03:04 +0530

    space

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,7 +44,7 @@ function App() {
 
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Users from Backend and deploy is workingg!!</h1>
+      <h1>Users from Backend and deploy is working!!</h1>
 
       {error && <p style={{ color: "red" }}>❌ {error}</p>}
 
